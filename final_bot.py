d463ba7a27602c1b9a079c1605251a407aa1c826
Abdulrahman-Taher <97679529+Abdulrahman-Taher@users.noreply.github.com>
2022-01-13 17:18:07 +0300

    Update final_bot.py

--- final_bot.py
+++ final_bot.py
@@ -3,8 +3,8 @@ import asyncio
 import telegram
 
 TELEGRAM_BOT_TOKEN = '5014571744:AAFBHU3UqfoUAFuv4ZXWv7j00r5mhLIdgDQ'
-TELEGRAM_CHAT_ID = '-1001239587631'
-PHOTO_PATH = 'https://drive.google.com/file/d/15lT0HEj8RX05Mrcx4lue9Nc5BVTdJWSE/view?usp=sharing'
+TELEGRAM_CHAT_ID = '-1001483409081'
+PHOTO_PATH = 'https://drive.google.com/file/d/13HOHISS1x6I9Kl6XnAAWXgFlYemH8TVQ/view?usp=sharing'
 FEED_URL = 'https://www.livechart.me/feeds/episodes'
 
 bot = telegram.Bot(token=TELEGRAM_BOT_TOKEN)
@@ -16,7 +16,24 @@ info1 = title.split("#")
 ti1 = info1[0][:-1]
 ep1 = info1[1]
 
-list = ['Petit Seka', 'BanG Dream! Garupa☆Pico: Fever!', 'Saiyuki RELOAD -ZEROIN-', 'Gal-gaku. II ~Lucky Stars~', 'Arifureta Shokugyou de Sekai Saikyou 2nd Season']
+my_list = """Kimetsu no Yaiba: Yuukaku-hen
+Shingeki no Kyojin: The Final Season Part 2
+Sono Bisque Doll wa Koi wo Suru
+BORUTO: NARUTO NEXT GENERATIONS
+Ousama Ranking
+SHAMAN KING (2021)
+Vanitas no Carte 2nd Cour
+Karakai Jouzu no Takagi-san 3
+Princess Connect! Re:Dive Season 2
+Akebi-chan no Sailor-fuku
+Slow Loop
+Dragon Quest: Dai no Daibouken (2020)
+One Piece
+Hakozume: Koban Joshi no Gyakushuu
+Meitantei Conan"""
+
+list = my_list.split("\n")
+
 async def main():
     await asyncio.sleep(60)
 
