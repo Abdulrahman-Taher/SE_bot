0cdfadbb5b5611a498907c82ea31243a368690a9
Abdulrahman-Taher <97679529+Abdulrahman-Taher@users.noreply.github.com>
2022-01-20 09:45:57 +0300

    Update final_bot.py

--- final_bot.py
+++ final_bot.py
@@ -16,23 +16,47 @@ info1 = title.split("#")
 ti1 = info1[0][:-1]
 ep1 = info1[1]
 
-my_list = """Kimetsu no Yaiba: Yuukaku-hen
-Shingeki no Kyojin: The Final Season Part 2
-Sono Bisque Doll wa Koi wo Suru
-BORUTO: NARUTO NEXT GENERATIONS
-Ousama Ranking
-SHAMAN KING (2021)
-Vanitas no Carte 2nd Cour
-Karakai Jouzu no Takagi-san 3
-Princess Connect! Re:Dive Season 2
-Akebi-chan no Sailor-fuku
+my_list = """Leadale no Daichi nite
+ORIENT
+Tokyo 24-ku
 Slow Loop
-Dragon Quest: Dai no Daibouken (2020)
+Shikkakumon no Saikyou Kenja
+SABIKUI BISCO
+Fantasy Bishoujo Juniku Ojisan to
+Koroshi Ai
+Kimetsu no Yaiba: Yuukaku-hen
+Shingeki no Kyojin: The Final Season Part 2
+Karakai Jouzu no Takagi-san 3
+Tensai Ouji no Akaji Kokka Saisei Jutsu
+Vanitas no Carte 2nd Cour
 One Piece
-Hakozume: Koban Joshi no Gyakushuu
-Meitantei Conan"""
-
+BORUTO: NARUTO NEXT GENERATIONS
+SHAMAN KING (2021)
+Meitantei Conan
+Dragon Quest: Dai no Daibouken (2020)
+Ousama Ranking"""
 list = my_list.split("\n")
+
+my_list2 = """#LeadaleNoDaichiNite
+#Orient
+#Tokyo24ku
+#SlowLoop
+#ShikkakumonNoSaikyouKenja
+#SabikuiBisco
+#FantasyBishoujoJunikuOjisanTo
+#KoroshiAi
+#KimetsuNoYaiba Yuukaku-hen
+#AttackOnTitan Final Season P2
+#KarakaiJouzuNoTakagiSan S3
+#TensaiOujiNoAkajiKokkaSaiseiJutsu
+#VanitasNoCarte 2nd Cour
+#OnePiece
+#Boruto
+#ShamanKing
+#DetectiveConan
+#DragonQuestDai
+#OusamaRanking"""
+list2 = my_list2.split("\n")
 
 async def main():
     await asyncio.sleep(60)
@@ -48,7 +72,10 @@ while 1:
         #print('if 1 is true')
         if ti2 in list:
             #print('if 2 is true')
-            cap = f'═────⊹⋘•♢•⋙⊹────═\n『' +ti2+ f'』┇Episode ' + ep2 + f'\n═────⊹⋘•♢•⋙⊹────═'
-            bot.send_photo(chat_id=TELEGRAM_CHAT_ID, photo=PHOTO_PATH, caption=cap)
+            ind = list.index(ti2)
+            #print(ind)
+            ti22 = list2[ind]
+            cap = f'═────⊹⋘•♢•⋙⊹────═\n『`%s`』┇EP%s\n═────⊹⋘•♢•⋙⊹────═'
+            bot.send_photo(chat_id=TELEGRAM_CHAT_ID, photo=PHOTO_PATH, caption=cap%(ti22, ep2), parse_mode="MARKDOWN")
     ti1 = ti2
     asyncio.run(main())
