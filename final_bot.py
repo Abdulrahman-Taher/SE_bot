10280e997fc0a1c226ad361c6d24d2b09cd3ea32
Abdulrahman-Taher <97679529+Abdulrahman-Taher@users.noreply.github.com>
2022-10-07 13:11:57 +0300

    Fall 2022 Update 1.0

--- final_bot.py
+++ final_bot.py
@@ -4,7 +4,7 @@ import telegram
 
 TELEGRAM_BOT_TOKEN = '5014571744:AAFBHU3UqfoUAFuv4ZXWv7j00r5mhLIdgDQ'
 TELEGRAM_CHAT_ID = '-1001483409081'
-PHOTO_PATH = 'https://drive.google.com/file/d/1LK7RJimIAbxZj01UWJzR8S1AkW4qgyeF/view?usp=sharing'
+PHOTO_PATH = 'https://drive.google.com/file/d/1o2-BndcxkDR0CV7LBiyddpuWjgDYl71-/view?usp=sharing'
 FEED_URL = 'https://www.livechart.me/feeds/episodes'
 
 bot = telegram.Bot(token=TELEGRAM_BOT_TOKEN)
@@ -22,38 +22,38 @@ my_list = """BORUTO: NARUTO NEXT GENERATIONS
 Dragon Quest: Dai no Daibouken (2020)
 Meitantei Conan
 One Piece
-Kingdom 4th Season
-Aoashi
-Summer Time Render
-Youkoso Jitsuryoku Shijou Shugi no Kyoushitsu e 2nd Season
-Engage Kiss
-Hataraku Maou-sama!!
-Isekai Ojisan
-Lycoris Recoil
-MADE IN ABYSS: Retsujitsu no Ougonkyo
-Overlord IV
-SHADOWS HOUSE 2nd Season
-Soredemo Ayumu wa Yosetekuru
-Yofukashi no Uta"""
+BLEACH: Sennen Kessen-hen
+Boku no Hero Academia 6th Season
+BLUELOCK
+Chainsaw Man
+Cool Doji Danshi
+Do It Yourself!!
+Fumetsu no Anata e Season 2
+Golden Kamuy 4th Season
+Meitantei Conan: Hannin no Hanzawa-san
+Kokyu no Karasu
+Mairimashita! Iruma-kun 3rd Season
+Mob Psycho 100 III
+SPY x FAMILY Cour 2"""
 list = my_list.split("\n")
 
 my_list2 = """#Boruto
 #DragonQuestDai
 #DetectiveConan
 #OnePiece
-#Kingdom S4
-#Aoashi
-#SummertimeRender
-#ClassroomOfTheElite S2
-#EngageKiss
-#HatarakuMaouSama!!
-#IsekaiOjisan
-#LycorisRecoil
-#MadeInAbyss
-#OverlordIV
-#ShadowsHouse S2
-#SoredemoAyumuWaYosetekuru
-#YofukashiNoUta"""
+#Bleach
+#BokuNoHero S6
+#BlueLock
+#ChainsawMan
+#CoolDojiDanshi
+#DoItYourself
+#FumetsuNoAnata S2
+#GoldenKamuy S4
+#HanninNoHanazawaSan
+#KoukyuuNoKarasu
+#Mairimashita S3
+#MobPsycho100 III
+#SpyxFamily P2"""
 list2 = my_list2.split("\n")
 
 async def main():
